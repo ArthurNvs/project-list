123b5c6098e7408f8b68db98749b8f9b234ee08a
Arthur Neves <arthurnvsm@gmail.com>
2022-06-25 07:57:23 -0300

    first commit

---
+++ projeto-lista/Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projeto-lista
+//
+//  Created by Arthur Monteiro on 25/06/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ projeto-lista/Screen/ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModel.swift
+//  projeto-lista
+//
+//  Created by Arthur Monteiro on 25/06/22.
+//
+
+import Foundation
+
+public class ViewModel {
+    
+}
+
+public protocol Screen {
+    
+    func didTapListCell()
+    
+}
